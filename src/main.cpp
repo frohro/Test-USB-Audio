718b4a4fa64a040924556a150c8711bcc10c286d
Rob Frohne <rob.frohne@wallawalla.edu>
2025-01-04 20:06:31 -0800

    Added a work around for what I think is a bug in the library, by reducing samples by one.

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,35 @@
+#include <cmath>
 #include "Adafruit_TinyUSB.h"
 #include "AudioTools.h"
 
 const float sampleRate = 48000;
-
-int16_t triangleWave = 0;
+const float frequencyLeft = 1000;
+const float frequencyRight = 1000;
+const float amplitude = 30000.0;
+const float twoPi = 2.0 * M_PI;
 
 Adafruit_USBD_Audio usb;
 
 size_t readCB(uint8_t* data, size_t len, Adafruit_USBD_Audio& ref) {
   int16_t* data16 = (int16_t*)data;
+  Serial.println(len);
   size_t samples = len / sizeof(int16_t);
+  Serial.println(samples);
   size_t result = 0;
-  static bool up = true;
-  Serial.printf("samples: %d\n", samples);
-  
-  for (size_t j = 0; j < samples; j += 2) {
-    if (up) {
-      triangleWave += 1000;
-      if (triangleWave >= 30000) {
-        up = false;
-      }
-    } else {
-      triangleWave -= 1000;
-      if (triangleWave <= 0) {
-        up = true;
-      }
-    }
-    Serial.printf("triangleWave: %d\n", triangleWave);
-    data16[j] = triangleWave; // Left channel  
-    data16[j + 1] = triangleWave; // Right channel
+  static float phaseLeft = 0.0;
+  static float phaseRight = 0.0;
+  float phaseIncrementLeft = twoPi * frequencyLeft / sampleRate;
+  float phaseIncrementRight = twoPi * frequencyRight / sampleRate;
+
+  for (size_t j = 0; j < samples-1; j += 2) {  // -1 is a work around for a bug in the library
+    data16[j] = amplitude * cos(phaseLeft); // Left channel
+    data16[j + 1] = amplitude * sin(phaseRight); // Right channel
+    phaseLeft += phaseIncrementLeft;
+    phaseRight += phaseIncrementRight;
+    if (phaseLeft >= twoPi) phaseLeft -= twoPi;
+    if (phaseRight >= twoPi) phaseRight -= twoPi;
     result += sizeof(int16_t) * 2;
   }
-  Serial.printf("result: %d\n", result);
   return result;
 }
 
