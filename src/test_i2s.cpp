0261dcfe05c2d22398c2ce8d02ae4ad2bc67a1a6
frohro <rob.frohne@wallawalla.edu>
2025-01-05 14:48:19 -0800

    test_i2s.cpp works

---
+++ src/test_i2s.cpp
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+#include <I2S.h>
+
+I2S i2s(INPUT);
+
+int32_t r[500], l[500];
+
+void setup()
+{
+
+    Serial.begin();
+
+    i2s.setDATA(2);
+    i2s.setBCLK(0);
+    i2s.setMCLK(3);
+    // Note: LRCK pin is BCK pin plus 1 (1 in this case).
+    i2s.setBitsPerSample(24);
+    i2s.setFrequency(8000);
+    i2s.setMCLKmult(384);
+    i2s.setSysClk(8000);
+    i2s.setBuffers(64, 16, 0);
+
+    i2s.begin();
+
+    //  while(1) {
+    //    int32_t l, r;
+    //    i2s.read24(&l, &r);
+    //    Serial.printf("%d %d\r\n", l, r);
+    //}
+}
+
+void loop()
+{
+    int i;
+    for (i = 0; i < 500; i++)
+    {
+        i2s.read32(l + i, r + i);
+    }
+    for (i = 0; i < 500; i++)
+    {
+        for (i = 0; i < 500; i++)
+        {
+            Serial.printf("> Right:%d, Left:%d\r\n", l[i] << 8, r[i] << 8);
+        }
+    }
+}
--- test/main.cpp
+++ test/main.cpp
@@ -5,7 +5,6 @@
 #include <I2S.h>
 
 I2S i2s;
-
 const int RATE = 48000;              // Your network needs to handle this, but 96000 should also work, but misses some packets.
 const int BITS_PER_SAMPLE_SENT = 16; // 24 or 32, 32 is less packet loss for some strange reason.
 const int MCLK_MULT = 256;  
@@ -14,18 +13,20 @@ Adafruit_USBD_Audio usb;
 
 size_t readCB(uint8_t* data, size_t len, Adafruit_USBD_Audio& ref) {
   int16_t* data16 = (int16_t*)data;
-  Serial.println(len);
+  // Serial.println(len);
   size_t samples = len / sizeof(int16_t);
-  Serial.println(samples);
+  // Serial.println(samples);
   size_t result = 0;
-  int32_t l, r;
+  int32_t r[samples], l[samples];
 
   for (size_t j = 0; j < samples-1; j += 2) {  // -1 is a work around for a bug in the library
-    i2s.read32(&l, &r);
-    data16[j] = l & 0xFFFF;       // Take the lower 16 bits of l
-    data16[j+1] = r & 0xFFFF;     // Take the lower 16 bits of r
+    int k = j/2;
+    i2s.read32(l + k, r + k);
+    data16[j] = (l[k] << 8) & 0xFFFF;       // Take the lower 16 bits of l
+    data16[j+1] = (r[k] << 8) & 0xFFFF;     // Take the lower 16 bits of r
     result += sizeof(int16_t) * 2;
   }
+  Serial.printf("l: %ld, r: %ld\n", l, r);
   return result;
 }
 
@@ -35,7 +36,7 @@ void setup() {
     i2s.setDATA(2); // These are the pins for the data on the SDR-TRX
     i2s.setBCLK(0);
     i2s.setMCLK(3);
-    // Note: LRCK pin is BCK pin plus 1 (1 in this case).
+    // Note: LRCK pin is always BCK pin plus 1 (1 in this case).
     i2s.setSysClk(RATE);
     i2s.setBitsPerSample(24);
     i2s.setFrequency(RATE);
